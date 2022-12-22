9c75ae48b854e168ea8eca9486458874b5c15721
Julidf <juliandiazfiano@gmail.com>
2022-12-22 12:56:59 -0300

    resolving CORS problem

--- api/src/main/java/com/restaurant/RestaurantApplication.java
+++ api/src/main/java/com/restaurant/RestaurantApplication.java
@@ -2,15 +2,28 @@ package com.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@ComponentScan({ "com.server", "com.server.config" })
-@SpringBootApplication(scanBasePackages = { "com.example.something", "com.restaurant" })
+@ComponentScan({ "com.restaurant", "com.restaurant.config" })
+@SpringBootApplication(scanBasePackages = { "com.restaurant", "com.restaurant.something" })
 
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }

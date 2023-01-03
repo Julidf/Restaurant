dce826419e828e4f8fd6b27030bd1b9b9566b7e0
Diego0014 <88014955+Diego0014@users.noreply.github.com>
2023-01-03 17:09:25 -0300

    Create product form created using Formik, it needs some adjustments

--- client/src/components/createProduct/createProduct.tsx
+++ client/src/components/createProduct/createProduct.tsx
@@ -1,23 +1,127 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import Swal from "sweetalert2";
+import validation from "./validation";
 
-export default function createProduct() {
-  const [state, setState] = useState({});
+
+export default function CreateProduct() {
+  const [product, setProduct] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    stock: 0,
+    image: "",
+  });
 
   const postProduct = async () => {
-    const response = await axios.post(
-      "http://localhost:8080/api/products",
-      state
-    );
-    setState(response.data);
+    await axios.post("http://localhost:8080/api/products", product);
   };
 
+  function submitButtonHandler(values: any) {
+    Swal.fire(JSON.stringify(values));
+    setProduct(values);
+    console.log(product);
+    postProduct();
+  }
+
   return (
-    <div>
-      <h1>Create Product</h1>
-      <form>
-        
-      </form>
-    </div>
+    <Formik
+      initialValues={product}
+      onSubmit={(values) => submitButtonHandler(values)}
+      validationSchema={validation}
+    >
+      {({ handleSubmit, values, handleChange }) => (
+        <Form className="form" onSubmit={handleSubmit}>
+          <label htmlFor="name" className="form__label">
+            Name:
+            <input
+              type="text"
+              name="name"
+              placeholder="insert name"
+              className="form__input"
+              value={values.name}
+              onChange={handleChange}
+            />
+            <ErrorMessage
+              name="name"
+              component="span"
+              className="form__error"
+            />
+          </label>
+
+          <label htmlFor="description" className="form__label">
+            Description:
+            <Field
+              type="text"
+              name="description"
+              placeholder="insert description"
+              className="form__input"
+              value={values.description}
+              onChange={handleChange}
+            />
+            <ErrorMessage
+              name="description"
+              component="span"
+              className="form__error"
+            />
+          </label>
+
+          <label htmlFor="price" className="form__label">
+            Price:
+            <Field
+              type="number"
+              step="0.01"
+              name="price"
+              placeholder="insert price"
+              className="form__input"
+              value={values.price}
+              onChange={handleChange}
+            />
+            <ErrorMessage
+              name="price"
+              component="span"
+              className="form__error"
+            />
+          </label>
+
+          <label htmlFor="stock" className="form__label">
+            Stock:
+            <Field
+              type="number"
+              name="stock"
+              placeholder="insert stock"
+              className="form__input"
+              value={values.stock}
+              onChange={handleChange}
+            />
+            <ErrorMessage
+              name="stock"
+              component="span"
+              className="form__error"
+            />
+          </label>
+
+          <label htmlFor="image" className="form__label">
+            Image:
+            <Field
+              type="text"
+              name="image"
+              placeholder="insert image"
+              className="form__input"
+              value={values.image}
+              onChange={handleChange}
+            />
+            <ErrorMessage
+              name="image"
+              component="span"
+              className="form__error"
+            />
+          </label>
+
+          <button type="submit" className="btn">Create</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
